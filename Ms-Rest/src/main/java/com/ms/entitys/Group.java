89bf55d5ef115a2b59b21a2f4db2dc5062c1ae1d
saddam <saddam@MSADDAM3.in.ibm.com>
2017-02-27 17:08:48 +0530

    Test 1

---
+++ Ms-Rest/src/main/java/com/ms/entitys/Group.java
@@ -0,0 +1,79 @@
+package com.ms.entitys;
+
+
+public class Group {
+
+	private int id;
+	private String category;
+	private String icon; 	// from fontawesome
+	private String bgColor; // from color picker
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public String getBgColor() {
+		return bgColor;
+	}
+	public void setBgColor(String bgColor) {
+		this.bgColor = bgColor;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bgColor == null) ? 0 : bgColor.hashCode());
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((icon == null) ? 0 : icon.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Group other = (Group) obj;
+		if (bgColor == null) {
+			if (other.bgColor != null)
+				return false;
+		} else if (!bgColor.equals(other.bgColor))
+			return false;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (icon == null) {
+			if (other.icon != null)
+				return false;
+		} else if (!icon.equals(other.icon))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return category;
+	}
+	
+	
+}
---
+++ Ms-Rest/src/main/java/com/ms/entitys/Iteam.java
@@ -0,0 +1,21 @@
+package com.ms.entitys;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Iteam {
+
+	private int id;
+	private Group group;
+	private String name;
+	private Date date;
+	
+	private void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
