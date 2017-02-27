ee8aa39b66a6e4305d7bdf54c6343e7777b43028
saddam <saddam@MSADDAM3.in.ibm.com>
2017-02-27 09:31:18 +0530

    Project Setup

---
+++ Ms-Rest/src/main/java/com/ibm/ms/MsRestApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRestApplication.class, args);
+	}
+}
